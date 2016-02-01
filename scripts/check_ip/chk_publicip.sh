83c17077da41daaf7f9308703697ffb68cb9c001
john.wang <john.wang@gandi.net>
2016-02-01 22:16:15 +0800

    check if the IP addr is valid and check the number of argument for check_public_ip function

--- scripts/check_ip/chk_publicip.sh
+++ scripts/check_ip/chk_publicip.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
 check_public_ip(){
-    echo "check public IP by site:$1"
-    curl $1
-    echo "\n--------------------------------\n"
+	#check if the number of argument is one.
+	if [ "$#" -ne 1 ] 
+	then
+		echo "invalid function call" >/dev/stderr
+		exit -1
+	fi
+	
+	#fetch the ip addr from a web service
+	pubip=$(curl $1 2>/dev/null)
+	
+	#check the IP if it is valid
+	if [[ $pubip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+	then
+		:
+	else
+		pubip="badip"
+	fi
+	
+	#report the public ip result
+	echo "$pubip $1"
 }
+
 
 check_public_ip ipecho.net/plain
 check_public_ip icanhazip.com
@@ -16,9 +34,7 @@ check_public_ip whatismyip.akamai.com
 check_public_ip tnx.nl/ip
 check_public_ip myip.dnsomatic.com
 check_public_ip ip.appspot.com
-check_public_ip ip.telize.com
 check_public_ip curlmyip.com
-
 
 #check IPv6
 #check_public_ip v6.ident.me
